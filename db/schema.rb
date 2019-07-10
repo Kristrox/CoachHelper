ef3b3cd2b258c6f09ba7fcf5553403948315266e
Kristrox <215914@edu.p.lodz.pl>
2019-07-10 10:03:40 +0200

    AddPasswordConstraintsAndTeamDisplay

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_09_191721) do
+ActiveRecord::Schema.define(version: 2019_07_10_071308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_09_191721) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "team", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
