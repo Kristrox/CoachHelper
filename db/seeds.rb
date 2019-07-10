77825ded1d6107e3b436ba6ef9cb181742ce8733
Kristrox <215914@edu.p.lodz.pl>
2019-07-10 10:35:40 +0200

    AddSeed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by(email: 'user@user.com', team: 'User Team') do |user|
+    user.password = '123456'
+  end
