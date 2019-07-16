2d7c6e5e0578ea62a7e9ec9975228a1e80405d37
Kristrox <215914@edu.p.lodz.pl>
2019-07-16 09:14:52 +0200

    AddClassesToPlayBooks

--- app/javascript/components/BottomToolBar.jsx
+++ app/javascript/components/BottomToolBar.jsx
@@ -3,6 +3,6 @@ import React, { Component } from "react";
 export default class BottomToolBar extends Component {
   state = {};
   render() {
-    return <h2>BottomToolBar</h2>;
+    return <h2 className="BottomToolBar">BottomToolBar</h2>;
   }
 }
