be19c8be0a34ea6ddeb4ee444aa67c4251f5a99c
Kristrox <215914@edu.p.lodz.pl>
2019-07-16 12:47:46 +0200

    AddPlayersTable

--- db/migrate/20190712080547_create_players.rb
+++ db/migrate/20190712080547_create_players.rb
@@ -3,7 +3,9 @@ class CreatePlayers < ActiveRecord::Migration[5.2]
     create_table :players do |t|
       t.string :name, null:false
       t.string :surname, null:false
+      t.integer :number, null:false , unique: true
       t.datetime :bith_date, null:false
+      t.integer :coach_id, null: false, unique: true
       t.boolean :trained_in_club, null:false
       t.boolean :trained_in_country, null:false
       t.boolean :european, null:false
