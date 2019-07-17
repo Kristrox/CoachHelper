8adb97224a6ca2691a1600ceb5a0535e57caa485
olga <olg262@gmail.com>
2019-07-17 13:03:22 +0200

    commit

---
+++ app/javascript/components/ball/Droppable/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export default class Droppable extends Component {
+  drop = e => {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("transfer");
+    this.props.updateBallPosition(this.props.x, this.props.y);
+  };
+
+  allowDrop = e => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <div
+        id={this.props.id}
+        onDrop={this.drop}
+        onDragOver={this.allowDrop}
+        style={this.props.style}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+Droppable.propTypes = {
+  id: PropTypes.string,
+  style: PropTypes.object,
+  children: PropTypes.node
+};
