3543a3b5185f958b13d31ce9c5056c30354f567a
pawelwelfle <pawel.welfle@gmail.com>
2019-07-17 22:28:56 +0200

    ValidationOfUniquenessOfNumber

--- app/models/player.rb
+++ app/models/player.rb
@@ -9,7 +9,8 @@ class Player < ApplicationRecord
     validates :yellow_cards, numericality: {less_than_or_equal_to: 4, greater_than_or_equal_to: 0}
     validates :number, :inclusion => 1..99, numericality: {only_inteager: true}
    # validate :expiration_date_cannot_be_in_the_past, on: :update
-
+    validates_uniqueness_of :number
+    
     before_create :default_yellow_cards, :default_red_cards # :default_end_of_contusion
    
     # def expiration_date_cannot_be_in_the_past
