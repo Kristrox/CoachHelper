bc0d3abc922a0538e260308f348163a536594658
Kamil Celejewski <kamil12283@gmail.com>
2019-07-20 14:27:00 +0200

    [add] finished players controller test, new calender and home controler test

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,10 +1,11 @@
 FactoryBot.define do
+    enum = [:club, :country, :europe, :world]
     factory :player do
         name { Faker::Games::LeagueOfLegends.champion }
         surname { Faker::Games::Pokemon.name }
         number { Faker::Number.between(10, 80) }
         birth_date {Faker::Date.between(50.years.ago, 15.years.ago) }
-        trained_in {Faker::Number.between(0, 3) } 
+        trained_in {enum[Faker::Number.between(0, 3)] } 
         red_cards {0}
         yellow_cards {0}
     end
