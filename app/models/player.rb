333af694c3a94f9c0d076220e1c86cf8a5aa6f58
pawelwelfle <pawel.welfle@gmail.com>
2019-07-22 10:13:27 +0200

    Delete - in PlayerModel

--- app/models/player.rb
+++ app/models/player.rb
@@ -7,6 +7,6 @@ class Player < ApplicationRecord
   validates :red_cards, numericality: { less_than_or_equal_to: 1, greater_than_or_equal_to: 0, only_integer: true }
   validates :yellow_cards, numericality: { less_than_or_equal_to: 4, greater_than_or_equal_to: 0, only_integer: true }
   validates :number, :inclusion => 1..99, numericality: {only_inteager: true}
-    -# validate :expiration_date_cannot_be_in_the_past, on: :update
+    # validate :expiration_date_cannot_be_in_the_past, on: :update
   validates_uniqueness_of :number
 end
