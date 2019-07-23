cce0e75804141b3c4c11ecf0a4857a0df5707651
Kristrox <215914@edu.p.lodz.pl>
2019-07-23 16:22:13 +0200

    FixMigrationsAndCombineIntoOne

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_17_194809) do
+ActiveRecord::Schema.define(version: 2019_07_00_071308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,7 @@ ActiveRecord::Schema.define(version: 2019_07_17_194809) do
     t.string "name", null: false
     t.string "surname", null: false
     t.integer "number", null: false
-    t.datetime "bith_date", null: false
+    t.datetime "birth_date", null: false
     t.integer "red_cards", default: 0, null: false
     t.integer "yellow_cards", default: 0, null: false
     t.datetime "created_at", null: false
