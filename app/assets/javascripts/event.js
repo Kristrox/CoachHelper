2f160b7d7ebe01d8a5eab394ff62ec6dcdc416c7
pawelwelfle <pawel.welfle@gmail.com>
2019-07-23 14:21:20 +0200

    FormatChangesInEventJS

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,5 +1,8 @@
 $(function () {
-    $('#datetimepicker2').datetimepicker({
-        format: 'YYYY-MM-DD, hh:mm'
+    $('#datetimepicker1').datetimepicker({
+        // format: 'YYYY-MM-DD, hh:mm'
+        format: 'YYYY-MM-DD HH:mm:ss'
+        // format: 'dddd, MMMM Do YYYY, HH:mm'
+        //format: 'YYYY-MM-DD'
     });
   });
