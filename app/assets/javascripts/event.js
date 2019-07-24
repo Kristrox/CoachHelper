623eaf4939331d40e1dd433aec0a442f3892a244
pawelwelfle <pawel.welfle@gmail.com>
2019-07-24 14:44:25 +0200

    DeleteSomeTrash

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,12 +1,3 @@
-// $(function () {
-//     $('#datetimepicker1').datetimepicker({
-//         // format: 'YYYY-MM-DD, hh:mm'
-//         format: 'YYYY-MM-DD HH:mm:ss'
-//         // format: 'dddd, MMMM Do YYYY, HH:mm'
-//         //format: 'YYYY-MM-DD'
-//     });
-//   });
-
   $(document).ready(function(){
     $('.datetimepicker4').datetimepicker({
         format: 'L',
