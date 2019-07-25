5ab5daf1698729176bf616c314c1d8c9d0517cdd
pawelwelfle <pawel.welfle@gmail.com>
2019-07-25 10:26:05 +0200

    ChangesInRspecTestsToEventController

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,14 +3,12 @@ require 'rails_helper'
 RSpec.describe Event, type: :model do
     describe 'attributes' do
         it 'should have proper attributes' do
-            expect(subject.attributes).to include('opponent', 'event_date', 'event_type', 'created_at', 'updated_at')
+            expect(subject.attributes).to include('opponent', 'event_date', 'created_at', 'updated_at')
         end
     end
 
     describe 'validations' do
         it { is_expected.to validate_presence_of(:opponent) }
         it { is_expected.to validate_presence_of(:event_date) }
-        it { is_expected.to validate_presence_of(:event_type) }
-        it { should define_enum_for(:event_type) }
     end
 end
