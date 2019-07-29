0040fa29027d9967b8749faa42858424a3ce522b
Kamil Celejewski <kamil12283@gmail.com>
2019-07-30 00:03:36 +0200

    [Add] play_books show test, gitingore /public/uploads/*, now if player is suspended his lable in players list will change color to red

---
+++ spec/controllers/play_books_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe PlayBooksController, type: :controller do
+
+    describe 'GET #show' do
+        let(:play_book) { create(:play_book) }
+        subject { get :show, params: { id: play_book.id }, format: :json }
+
+        describe 'succesfull response' do
+            before{ subject }
+            it { expect(response).to be_successful } 
+            it { expect(response).to render_template('show') }
+        end
+    end
+end
