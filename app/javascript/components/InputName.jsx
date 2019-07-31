7c18179f3917893cc2171abc72b7e333fab839c9
Kamil Celejewski <kamil12283@gmail.com>
2019-08-01 00:11:10 +0200

    [Fix] play book form

--- app/javascript/components/InputName.jsx
+++ app/javascript/components/InputName.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const InputName = props => {
   return (
-    <div className="form-group">
-      <label htmlFor="formGroupExampleInput">Name</label>
+    <div className="form-group d-flex flex-row">
+      <label className="servName" htmlFor="formGroupExampleInput">Name</label>
       <input
         type="text"
         className="form-control"
         value={props.name}
+        size={8}
         onChange={e => props.onChangeName(e.target.value)}
       />
     </div>
